bc14fc97acefc70aaa4dfc4949048ba731a4c7ef
Todd Albert <todd@bocacode.com>
2022-11-02 10:20:24 -0400

    added routes

---
+++ src/scenes/AddAlbum.jsx
@@ -0,0 +1,8 @@
+export default function AddAlbum() {
+  return (
+    <>
+      <h1>Add Album Title goes here</h1>
+      <p>New Album Form goes here</p>
+    </>
+  )
+}
---
+++ src/scenes/SingleAlbum.jsx
@@ -0,0 +1,8 @@
+export default function SingleAlbum() {
+  return (
+    <>
+      <h1>Album Title goes here</h1>
+      <p>Album Card goes here</p>
+    </>
+  )
+}
